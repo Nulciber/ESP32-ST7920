6cd058599a4cad73225c0605a3dd64b8f14bf919
Nulciber <Nulciber@protonmail.com>
2026-05-17 22:51:12 +0200

    test c

---
+++ test_pc/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void afficherBitsStandard(uint8_t valeur)
+{
+    for (int i = 7; i >= 0; i--)
+        printf("%d", (valeur >> i) & 1);
+    printf("  (0x%02x = %d)", valeur, valeur);
+}
+
+
+
+int main()
+{
+    uint8_t a = 0x36;
+    uint8_t b = 0xF0;
+
+    printf("a         = "); afficherBitsStandard(a); printf("\n");
+    printf("b         = "); afficherBitsStandard(b); printf("\n");
+    printf("a & b     = "); afficherBitsStandard(a & b); printf("\n");
+    printf("a | b     = "); afficherBitsStandard(a | b); printf("\n");
+    printf("a ^ b     = "); afficherBitsStandard(a ^ b); printf("\n");
+    printf("a << 4    = "); afficherBitsStandard(a << 4); printf("\n");
+    printf("a >> 4    = "); afficherBitsStandard(a >> 4); printf("\n");
+    printf("~a        = "); afficherBitsStandard(~a); printf("\n");
+
+    return 0;
+}
---
+++ test_pc/testc_pab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    uint8_t a = 0x36;
+    
+    printf("%d",a);
+    //printf("Voici le nombre en hexa:");printf("%02x\n", a);
+    //printf("Voici le nombre en decimal:");printf("%02d\n", a);
+    //printf("Voici le nombre en binaire:");printf("%08b\n", a<<2);
+    return 0;
+}
